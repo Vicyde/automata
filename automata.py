3d20e65b225babeb2056fd9f6c1dd77f72c85a37
Jasper Reuhman <jreuhman@fastmail.nl>
2021-10-08 15:00:00 +0200

    First Commit

---
+++ automata.py
@@ -0,0 +1,55 @@
+import sys
+
+def print_row(row):
+    for x in row:
+        if x == 1:
+            print("*", end ='')
+        else:
+            print("-", end='')
+
+    print()
+
+def calculate_row(row, rule):
+    rule = "{:08b}".format(rule)
+    ruleset = []
+    newrow = []
+
+    for n in range(8):
+        ruleset.append(int(rule[7-n]))
+
+    for x in range(len(row)):
+        xl = 0 if x == 0 else row[x-1]             # if at the left edge, xl is 0. Else it is the value in x-1
+        xr = 0 if x >= len(row) - 1 else row[x+1]   # if at the right edge, xr = 0. Else it is the value in x+1
+        xc = row[x]
+
+        # convert the binary xl, xc, xr to a number
+        val = (xl << 2) + (xc << 1) + xr
+        newrow.append(ruleset[val])
+
+    return newrow
+
+def main(argv):
+    rule = 90
+    height = 80
+
+    if len(argv) > 1:
+        for arg in range(1, len(argv)):
+            if argv[arg] == '-r':
+                arg += 1
+                rule = int(arg)
+            if argv[arg] == '-h':
+                arg += 1
+                height = int(argv[arg])
+
+    row = [ 0 ] * 64
+    row[32] = 1
+
+    for n in range(height):
+        print_row(row)
+        row = calculate_row(row, rule)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+    
